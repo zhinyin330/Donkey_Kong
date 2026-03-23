982ddffe63b1e1c0017db859ca780288691e54ce
zhinyin330 <zzhiyi98@gmail.com>
2026-03-23 09:10:45 +0100

    33

--- Game_Donkey_Kong/src/Game.cpp
+++ Game_Donkey_Kong/src/Game.cpp
@@ -1,16 +1,14 @@
 ﻿#include "Game.h"
 #include "resource_dir.h"
+#include "player.h"
 
 void RunGameLoop()
 {
     // 假设你窗口已经初始化
     bool gameOver = false;
-
+    Player player;
+    player.Init();
     // 加载游戏资源
-    Texture player = LoadTexture("Characters/Mario/Dk_Mario_Idle1.png");
-    Rectangle sourceRec1 = { 0, 0, (float)player.width, (float)player.height }; // 原图大小
-    Rectangle destRec1 = { 110, 300, 50, 50 };
-    Vector2 origin1 = { 0, 0 };
 
     Texture dongkeyKong = LoadTexture("Characters/DonkeyKong/Dk_DonkeyKong_BarrelGrab_M.png");;
     Rectangle sourceRec2 = { 0, 0, (float)dongkeyKong.width, (float)dongkeyKong.height }; // 原图大小
@@ -23,9 +21,11 @@ void RunGameLoop()
 
         BeginDrawing();
         ClearBackground(BLACK);
+        player.Update();
+
+        player.Draw();
 
         // 绘制游戏元素
-        DrawTexturePro(player, sourceRec1, destRec1, origin1, 0.0f, WHITE);
         DrawTexturePro(dongkeyKong, sourceRec2, destRec2, origin2, 0.0f, WHITE);
 
 
@@ -33,6 +33,5 @@ void RunGameLoop()
     }
 
     // 卸载资源
-    UnloadTexture(player);
     UnloadTexture(dongkeyKong);
 }
