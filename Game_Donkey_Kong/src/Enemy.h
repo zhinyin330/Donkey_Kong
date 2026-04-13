39a103c31b46f5c7d6efad5bf764e4d47ec9493a
zhinyin330 <zzhiyi98@gmail.com>
2026-04-13 09:52:04 +0200

    Enemy
    Enemy and jump audio

---
+++ Game_Donkey_Kong/src/Enemy.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include "raylib.h"
+#include <vector>
+
+// 敌人状态（目前只有一个动画，后面可以扩展）
+enum class EnemyState {
+    BARREL_GRAB
+};
+
+class Enemy {
+private:
+    // ===== 贴图 =====
+    std::vector<Texture2D> barrelGrabTextures;
+    Texture2D currentTexture;
+
+    // ===== 动画 =====
+    EnemyState currentState;
+    int currentFrame;
+    float frameCounter;
+    float frameSpeed;
+
+    // ===== 位置 & 属性 =====
+    Vector2 position;
+    float scale;
+    bool facingRight;
+
+public:
+    Enemy();
+    ~Enemy();
+
+    // ===== 核心逻辑 =====
+    void Update();
+    void Draw();
+
+    // ===== 动画控制 =====
+    void UpdateAnimation();
+    void ChangeState(EnemyState newState);
+};
--- Game_Donkey_Kong/src/Game.cpp
+++ Game_Donkey_Kong/src/Game.cpp
@@ -2,6 +2,7 @@
 #include "resource_dir.h"
 #include "Player.h"
 #include "Scene.h"
+#include "Enemy.h"
 
 void RunGameLoop()
 {
@@ -9,17 +10,14 @@ void RunGameLoop()
     bool gameOver = false;
 
     InitWindow(Scene::GetScreenWidth(), Scene::GetScreenHeight(), "Donkey Kong");
-    
+
+    InitAudioDevice();//audio
     Scene scene;
     Player player;
+    Enemy enemy;
     SetTargetFPS(60);
 
-    // 加载游戏资源
-
-    Texture dongkeyKong = LoadTexture("Characters/DonkeyKong/Dk_DonkeyKong_BarrelGrab_M.png");;
-    Rectangle sourceRec2 = { 0, 0, (float)dongkeyKong.width, (float)dongkeyKong.height }; // 原图大小
-    Rectangle destRec2 = { 110, 400, 130, 80 };
-    Vector2 origin2 = { 0, 0 };
+   
     while (!WindowShouldClose() && !gameOver)
     {
         
@@ -27,6 +25,7 @@ void RunGameLoop()
         // UPDATE
         player.HandleInput();
         player.Update(scene);
+        enemy.Update();
 
 
         // 更新游戏逻辑
@@ -37,15 +36,11 @@ void RunGameLoop()
 
         scene.Draw();
         player.Draw(); 
-
-        // 绘制游戏元素
-        DrawTexturePro(dongkeyKong, sourceRec2, destRec2, origin2, 0.0f, WHITE);
+        enemy.Draw();
 
 
         EndDrawing();
 
     }
 
-    // 卸载资源
-    UnloadTexture(dongkeyKong);
 }
