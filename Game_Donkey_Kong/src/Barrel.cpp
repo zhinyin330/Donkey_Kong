71c272cfc9b31da47d01c4648a22a59a405fc342
zhinyin330 <zzhiyi98@gmail.com>
2026-05-13 12:00:31 +0200

    22

--- Game_Donkey_Kong/src/Barrel.cpp
+++ Game_Donkey_Kong/src/Barrel.cpp
@@ -33,7 +33,7 @@ Barrel::Barrel(BarrelType t, Vector2 pos)
     LoadFrames();
 
      
-    //  DROP 触发点 进入纵向下落
+    //  DROP 触发点 进入纵向下落1
     verticalDropTriggers.push_back({ {635.0f, 205.0f, 32.0f, 32.0f},TriggerType::DROP, 0 });
     verticalDropTriggers.push_back({ {125.0f,305.0f, 32.0f, 32.0f}, TriggerType::DROP, 0 });
     verticalDropTriggers.push_back({ {635.0f,400.0f, 32.0f, 32.0f}, TriggerType::DROP, 0 });
@@ -41,12 +41,22 @@ Barrel::Barrel(BarrelType t, Vector2 pos)
     verticalDropTriggers.push_back({ {635.0f,595.0f, 32.0f, 32.0f}, TriggerType::DROP, 0 });
 
     // RETURN 触发恢复横向移动
-    // direction 方向控制
+    // direction 方向控制2
     verticalDropTriggers.push_back({ {650.0f,295.0f, 32.0f, 32.0f}, TriggerType::RETURN, -1 });
     verticalDropTriggers.push_back({ {140.0f,390.0f, 32.0f, 32.0f}, TriggerType::RETURN,  1 });
     verticalDropTriggers.push_back({ {650.0f,490.0f, 32.0f, 32.0f}, TriggerType::RETURN, -1 });
     verticalDropTriggers.push_back({ {140.0f,585.0f, 32.0f, 32.0f}, TriggerType::RETURN,  1 });
     verticalDropTriggers.push_back({ {650.0f,675.0f, 32.0f, 32.0f}, TriggerType::RETURN, -1 });
+    
+    // direction 方向控制3
+    verticalDropTriggers.push_back({ {255.0f,305.0f, 32.0f, 32.0f}, TriggerType::DROP, 0 });//有几率触发
+    verticalDropTriggers.push_back({ {405.0f,400.0f, 32.0f, 32.0f}, TriggerType::DROP, 0 });//有几率触发
+    verticalDropTriggers.push_back({ {355.0f,495.0f, 32.0f, 32.0f}, TriggerType::DROP, 0 });//有几率触发
+    // direction 方向控制4
+    verticalDropTriggers.push_back({ {270.0f,390.0f, 32.0f, 32.0f}, TriggerType::RETURN,  1 });
+    verticalDropTriggers.push_back({ {420.0f,490.0f, 32.0f, 32.0f}, TriggerType::RETURN, -1 });
+    verticalDropTriggers.push_back({ {370.0f,585.0f, 32.0f, 32.0f}, TriggerType::RETURN,  1 });
+
 }
 
 // 加载动画
